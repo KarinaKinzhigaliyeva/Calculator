d43246ed9d1100e29a2d3bf3e5be11a071d1b87b
Karina Kinzhigaliyeva <karinakinzhigaliyeva@gmail.com>
2024-09-20 15:33:29 +0500

    Initial Commit

---
+++ myFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Karina Kinzhigaliyeva on 20.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
